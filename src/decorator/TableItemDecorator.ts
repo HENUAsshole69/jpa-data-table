4fb336a1d4b7ce047e02b063edb57486eff531a1
Administrator <qq844303749@gmail.com>
2020-07-29 16:18:17 +0800

    import fix

--- src/decorator/TableItemDecorator.ts
+++ src/decorator/TableItemDecorator.ts
@@ -1,11 +1,11 @@
-import  "reflect-metadata";
-import {TableItem} from "@/model/TableItem";
+import "reflect-metadata";
+import {TableItem} from "../model/TableItem";
 
 export const tableItemMetadataKey = Symbol("TableItem");
 
 export function TableItem(tableItem: TableItem): ClassDecorator {
-    return (target)=>{
-        Reflect.defineMetadata(tableItemMetadataKey ,tableItem,target.prototype);
+    return (target) => {
+        Reflect.defineMetadata(tableItemMetadataKey, tableItem, target.prototype);
         return target
     }
 }
